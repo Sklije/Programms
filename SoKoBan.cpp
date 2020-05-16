b97b7480c2eaf7664de8870d99ae53343a9d7bc7
Sklije <61091591+Sklije@users.noreply.github.com>
2020-05-16 20:12:23 +0300

    Update SoKoBan.cpp

--- SoKoBan.cpp
+++ SoKoBan.cpp
@@ -4,7 +4,7 @@
 #include <iomanip>
 #include <vector>
 #include <string>
-
+#include <fstream>
 
 using namespace std;
 
@@ -19,71 +19,120 @@ struct Point {
 	}
 };
 
-struct Map {
-	Point Loader;
-	vector<string>* map;
-	int Boxes;
-} Lvl1;
 
-
-
-void Move(char Direction, Point& Loader) {
-	if (Direction == 'w' && (*Lvl1.map)[Loader.y - 1][Loader.x] != '*') { Loader.y -= 1; }
-	else if (Direction == 's' && (*Lvl1.map)[Loader.y + 1][Loader.x] != '*') { Loader.y += 1; }
-	else if (Direction == 'a' && (*Lvl1.map)[Loader.y][Loader.x - 1] != '*') { Loader.x -= 1; }
-	else if (Direction == 'd' && (*Lvl1.map)[Loader.y][Loader.x + 1] != '*') { Loader.x += 1; }
+bool It_Is_A_Box(Point* Box, int X, int Y, int Boxes) {
+	for (int i = 0; i < Boxes; i++) {
+		if (Box[i].x == X && Box[i].y == Y) {
+			return true;
+		}
+	}
+	return false;
 }
 
-void Drow(Point& Loader) {
-	for (int i = 0; i < 9; i++) {
-		for (int j = 0; j < 8; j++) {
-			if (Loader.y == i && Loader.x == j) {
+
+void Move(char Direction, Point& Loader, string* Map, Point* Box, int Boxes) {
+	int offset = 1;
+	int num = -1;
+	for (int i = 0; i < Boxes; i++) {
+		if (Direction == 'w' && It_Is_A_Box(Box, Loader.x, Loader.y - 1, Boxes)/*Loader.y - 1 == Box[i].y && Loader.x == Box[i].x*/) {
+			offset = 2;
+			num = i;
+		}
+		else if (Direction == 's' && It_Is_A_Box(Box, Loader.x, Loader.y + 1, Boxes)/*Loader.y + 1 == Box[i].y && Loader.x == Box[i].x*/) {
+			offset = 2;
+			num = i;
+		}
+		else if (Direction == 'a' && It_Is_A_Box(Box, Loader.x - 1, Loader.y, Boxes)/*Loader.y == Box[i].y && Loader.x - 1 == Box[i].x*/) {
+			offset = 2;
+			num = i;
+		}
+		else if (Direction == 'd' && It_Is_A_Box(Box, Loader.x + 1, Loader.y, Boxes)/*Loader.y == Box[i].y && Loader.x + 1 == Box[i].x*/) {
+			offset = 2;
+			num = i;
+		}
+		else offset = 1;
+	}
+
+	if (Direction == 'w' && (Map[Loader.y - offset][Loader.x] == '.' || Map[Loader.y - offset][Loader.x] == 'p')) {
+		Loader.y -= 1;
+		if (offset == 2) Box[num].y -= 1;
+	}
+	else if (Direction == 's' && (Map[Loader.y + offset][Loader.x] == '.' || Map[Loader.y + offset][Loader.x] == 'p')) {
+		Loader.y += 1;
+		if (offset == 2) Box[num].y += 1;
+	}
+	else if (Direction == 'a' && (Map[Loader.y][Loader.x - offset] == '.' || Map[Loader.y][Loader.x - offset] == 'p')) {
+		Loader.x -= 1;
+		if (offset == 2) Box[num].x -= 1;
+	}
+	else if (Direction == 'd' && (Map[Loader.y][Loader.x + offset] == '.' || Map[Loader.y][Loader.x + offset] == 'p')) {
+		Loader.x += 1;
+		if (offset == 2) Box[num].x += 1;
+	}
+	offset = 1;
+	num = -1;
+}
+
+void Push() {
+
+}
+
+void Draw(Point& Loader, string* Map, Point* Box, int X, int Y, int Boxes) {
+	for (int i = 0; i < Y; i++) {
+		for (int j = 0; j < X; j++) {
+			if (i == Loader.y && j == Loader.x) {
 				cout << setw(2) << "G";
 			}
-			else {
-				cout << setw(2) << (*Lvl1.map)[i][j];
+			else if (It_Is_A_Box(Box, j, i, Boxes)) {
+				cout << setw(2) << "B";
 			}
+			else cout << setw(2) << Map[i][j];
 		}
 		cout << endl;
 	}
 }
+
+
 int main() {
 	//	SetConsoleCP(1251);
 	//	SetConsoleOutputCP(1251);
 
-	Lvl1.map = new vector<string>{
-		"******..",
-		"*p*..**.",
-		"*ppp..**",
-		"*.*B...*",
-		"*.B.*..*",
-		"**.B.B.*",
-		".*B.B.**",
-		".*...**.",
-		".*****.."
-	};
-	Lvl1.Loader.x = 3;
-	Lvl1.Loader.y = 4;
+	int X = 0, Y = 0;
+	string* Map = nullptr;
+	Point Loader;
+	Loader.x = 0, Loader.y = 0;
+	int Boxes = 0;
+	Point* Box = nullptr;
+	int Points = 0;
 
-	Point Loader = Lvl1.Loader;
-	char key;
-	/*for (int i = 0; i < 9; i++) {
-		for (int j = 0; j < 8; j++) {
-			if ((*Lvl1.map)[i][j] == 'G') {
-				Loader.y = i;
-				Loader.x = j;
-			}
+	int LevelChoise;
+	cin >> LevelChoise;
+	if (LevelChoise == 1) {
+		ifstream input("Lvl1.txt");
+		input >> X >> Y;
+		Map = new string[Y];
+		for (int i = 0; i < Y; i++) {
+			input >> Map[i];
 		}
-	}*/
-	int krya = 1;
-	Drow(Loader);
-	while (krya != 0) {
+		input >> Loader.x >> Loader.y;
+		input >> Boxes;
+		Box = new Point[Boxes];
+		for (int i = 0; i < Boxes; i++) {
+			input >> Box[i].x >> Box[i].y;
+		}
+		input >> Points;
+	}
+	system("cls");
+	char key;
+	Draw(Loader, Map, Box, X, Y, Boxes);
+
+	while (Points > 0 || key) {
 		key = _getch();
 		system("cls");
-
-		Move(key, Loader);
-		Drow(Loader);
-
+		Move(key, Loader, Map, Box, Boxes);
+		Draw(Loader, Map, Box, X, Y, Boxes);
 	}
+
+
 	return 0;
 }
